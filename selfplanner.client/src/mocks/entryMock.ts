ba58218df1b8f0f5587544bc507147713a00931a
AxAnder0095 <doriscafe@hotmail.com>
2026-03-07 12:30:21 -0600

    mock data

---
+++ selfplanner.client/src/mocks/entryMock.ts
@@ -0,0 +1,11 @@
+interface Entry {
+    id: string;
+    name: string,
+    age: number
+};
+
+export const entryMock: Entry[] = [
+    { id: "1", name: "Test Name 1", age: 25 },
+    { id: "2", name: "Test Name 2", age: 30 },
+    { id: "3", name: "Test Name 3", age: 35 }
+];
